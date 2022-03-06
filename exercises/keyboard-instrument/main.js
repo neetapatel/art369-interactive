d792736b3ed19d09b7b6688800e49366548a32d1
neetapatel <npfluter@gmail.com>
2022-03-06 13:50:03 -0500

    keyboard exercise

---
+++ exercises/keyboard-instrument/main.js
@@ -0,0 +1,141 @@
+// this is where you write your javascript
+
+// rosa's code 
+
+// $(window).keydown(function(e){
+
+//   var key = e.key;
+
+//   console.log(key);
+
+//   if (key == "a") {
+//     $(".message").html("Apple");
+//   }
+//   else if (key == "b") {
+//     $(".message").html("Bagpipes");
+//   }
+//   else if (key == "c") {
+//     $(".message").html("Cadillac");
+//   } 
+
+// });
+
+// event listener
+$(window).keydown(function(e){ // e is a way to refer to the event that's happening. 
+  var key = e.key; // target the event's key attribute
+  console.log(key);
+
+  // if (key == "a") {$(".a").toggle();}
+  if (key == "a") {
+    $(".item").css("opacity", 0);
+    $(".a").css("opacity", 1);
+  }
+  else if (key == "b") {
+    $(".item").css("opacity", 0);
+    $(".b").css("opacity", 1);
+  }
+  else if (key == "c") {
+    $(".item").css("opacity", 0);
+    $(".c").css("opacity", 1);
+  }
+  else if (key == "d") {
+    $(".item").css("opacity", 0);
+    $(".d").css("opacity", 1);
+  }
+  else if (key == "e") {
+    $(".item").css("opacity", 0);
+    $(".e").css("opacity", 1);
+  }
+  else if (key == "f") {
+    $(".item").css("opacity", 0);
+    $(".f").css("opacity", 1);
+  }
+  else if (key == "g") {
+    $(".item").css("opacity", 0);
+    $(".g").css("opacity", 1);
+  }
+  else if (key == "h") {
+    $(".item").css("opacity", 0);
+    $(".h").css("opacity", 1);
+  }
+  else if (key == "i") {
+    $(".item").css("opacity", 0);
+    $(".i").css("opacity", 1);
+  }
+  else if (key == "j") {
+    $(".item").css("opacity", 0);
+    $(".j").css("opacity", 1);
+  }
+  else if (key == "k") {
+    $(".item").css("opacity", 0);
+    $(".k").css("opacity", 1);
+  }
+  else if (key == "l") {
+    $(".item").css("opacity", 0);
+    $(".l").css("opacity", 1);
+  }
+  else if (key == "m") {
+    $(".item").css("opacity", 0);
+    $(".m").css("opacity", 1);
+  }
+  else if (key == "n") {
+    $(".item").css("opacity", 0);
+    $(".n").css("opacity", 1);
+  }
+  else if (key == "o") {
+    $(".item").css("opacity", 0);
+    $(".o").css("opacity", 1);
+  }
+  else if (key == "p") {
+    $(".item").css("opacity", 0);
+    $(".p").css("opacity", 1);
+  }
+  else if (key == "q") {
+    $(".item").css("opacity", 0);
+    $(".q").css("opacity", 1);
+  }
+  else if (key == "r") {
+    $(".item").css("opacity", 0);
+    $(".r").css("opacity", 1);
+  }
+  else if (key == "s") {
+    $(".item").css("opacity", 0);
+    $(".s").css("opacity", 1);
+  }
+  else if (key == "t") {
+    $(".item").css("opacity", 0);
+    $(".t").css("opacity", 1);
+  }
+  else if (key == "u") {
+    $(".item").css("opacity", 0);
+    $(".u").css("opacity", 1);
+  }
+  else if (key == "v") {
+    $(".item").css("opacity", 0);
+    $(".v").css("opacity", 1);
+  }
+  else if (key == "w") {
+    $(".item").css("opacity", 0);
+    $(".w").css("opacity", 1);
+  }
+  else if (key == "x") {
+    $(".item").css("opacity", 0);
+    $(".x").css("opacity", 1);
+  }
+  else if (key == "y") {
+    $(".item").css("opacity", 0);
+    $(".y").css("opacity", 1);
+  }
+  else if (key == "z") {
+    $(".item").css("opacity", 0);
+    $(".z").css("opacity", 1);
+  }
+  else if (key == ",") {
+    $(".item").css("opacity", 0);
+    $(".comma").css("opacity", 1);
+  }
+  else {
+    $(".item").css("opacity", 0);
+  } 
+
+});
