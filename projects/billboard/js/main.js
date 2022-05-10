8f0d133df8d71d4106456913eb189385d42092e5
neetapatel <npfluter@gmail.com>
2022-05-09 21:04:35 -0400

    billboard update

--- projects/billboard/js/main.js
+++ projects/billboard/js/main.js
@@ -12,16 +12,13 @@ var base = Airtable.base('appiC58DhMQ4qhuiF');
 // set up a blank array for all your rows
 const rows = [];
 
-// set up color arrays
+// set up a blank duplicate array for shuffling
+const deck = [];
 
+// set up color arrays
 const bgColors = ['rgb(235,74,39)', 'rgb(56,126,247)', 'rgb(74,163,173)', 'rgb(238,121,47)', 'rgb(232,233,85)', 'rgb(118,234,252)', 'rgb(234,59,36)', 'rgb(68,153,43)', 'rgb(43,101,180)', 'rgb(187,38,69)', 'rgb(111, 162, 56)', 'rgb(235,77,193)', 'rgb(228,161,56)', 'rgb(79,175,249)'];
 const txtColors = ['rgb(56,126,247)', 'rgb(235,74,39)', 'rgb(238,121,47)', 'rgb(74,163,173)', 'rgb(118,234,252)', 'rgb(232,233,85)', 'rgb(68,153,43)', 'rgb(234,59,36)', 'rgb(187,38,69)', 'rgb(43,101,180)', 'rgb(235,77,193)', 'rgb(111, 162, 56)', 'rgb(79,175,249)', 'rgb(228,161,56)'];
 
-// const bgColors = ['red', 'lime', 'blue', 'orangered', 'lime', 'magenta', 'yellow', 'cyan', 'magenta', 'blue', 'lime', 'blue', 'orangered', 'dodgerblue'];
-// const txtColors = ['lime', 'red', 'orangered', 'blue', 'magenta', 'lime', 'cyan', 'yellow', 'blue', 'magenta', 'blue', 'lime', 'dodgerblue', 'orangered'];
-
-// const bgColors = ['red', 'orangered', 'orange', 'yellow', 'lime', 'green', 'cyan', 'blue', 'indigo', 'darkviolet', 'mediumvioletred', 'purple', 'darkorchid', 'deeppink', 'magenta'];
-// const txtColors = ['lime', 'darkorchid', 'cyan', 'magenta', 'yellow', 'purple', 'orange', 'orangered',  'red', 'green',  'blue', 'deeppink', 'mediumvioletred', 'darkviolet', 'cyan'];
 var totalColors = bgColors.length;
 
 // this line of code says to get all the info from AirTable
@@ -49,16 +46,22 @@ function gotAllRows(err) {
         console.error(err);
         return;
     }
-    consoleLogRows();
+    // consoleLogRows();
+
+    // duplicate data
+    for (let i = 0; i < rows.length; i ++) deck[i] = rows[i];
+    // shuffle deck
+    for (let i = rows.length - 1; i >= 0; i--) {
+        let j = Math.floor(Math.random() * deck.length);
+        let temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
+    }
 
     // here, we want to show the first record only, 
     // which happens with the showRow function.
-    // showRow(rows[index]);
-    // var rand = getRandomIntInclusive(1, rows.length);
-    showRow(rows[getRandomIntInclusive(1, rows.length)]);
+    showRow(deck[0]);
 }
-
-var index = 0;
 
 // consoleLogRows simply logs each row to the console, 
 // so you can see it's data and fields.
@@ -70,22 +73,15 @@ function consoleLogRows() {
     console.log(rows.length);
 }
 
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
-  }
-
 // showRows is what puts the content onto the HTML page.
 // it only displays a single row.
 function showRow(row) {
-
     console.log("showRow()");
 
     const slide = document.getElementById("slide");
 
-    var value = getRandomIntInclusive(0, totalColors-1);
-
+    var value = Math.floor(Math.random() * totalColors);
+    
     document.body.style.backgroundColor = bgColors[value];
     document.getElementById("logo").style.display = "none";
     document.getElementById("slide-text").style.color = txtColors[value];
@@ -99,42 +95,21 @@ function showRow(row) {
     let root = document.documentElement;
     root.style.setProperty('--width', w + "px");
     root.style.setProperty('--height', h + "px");   
-
-    console.log("height: " + h);
-    console.log("width: " + w);
-
-
 }
 
-
-
-
-
-// This sets up an event.
-// When you click on a slide, you replace the content with the message
-// form the next row. 
-// Some additional logic ensures that when you reach the last row,
-// you loop back to the beginning.
+var index = 0;
 
 function main() {
-    var rand = getRandomIntInclusive(1, rows.length);
-    console.log(rand);
-    showRow(rows[rand]);
+    console.log("index: " + index);
+    if (index < deck.length - 1) {
+        index = index + 1;
+        showRow(deck[index]);
+    } else {
+        index = 0;
+        showRow(deck[index]);
+    }
 }
-setInterval(main, 13000);
-
-
-// document.getElementById("slide-text").addEventListener("click", () => {
-//     // console.log(index);
-//     showRow(rows[getRandomIntInclusive(1, rows.length)]);
-//     // if (index < rows.length - 1) {
-//     //     index = index + 1;
-//     //     showRow(rows[index]);
-//     // } else {
-//     //     index = 0;
-//     //     showRow(rows[index]);
-//     // }
-// })
+setInterval(main, 13000); 
 
 function pop() {
     window.open("https://airtable.com/shrydM5VTPprGXiSJ", "Submission", "width=500,height=600");
