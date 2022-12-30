36974eeaf80727f9924a4ff528d2a2e99dd73f60
neetapatel <npfluter@gmail.com>
2022-12-30 12:35:15 -0500

    coffee timer update

--- projects/tool/js/myScript.js
+++ projects/tool/js/myScript.js
@@ -12,8 +12,9 @@ function setTime() { // onclick
   document.getElementById("start-btn").style.padding = "0";
   document.getElementById("start-btn").disabled = true;
   document.getElementById("start-btn").style.cursor = "auto";
-  document.getElementById("start-btn").style.color = "chocolate";
-  goDark();
+  document.getElementById("start-btn").style.color = "var(--light-color)";
+  document.getElementById("start-btn").style.fontSize = "2rem";
+  goLight();
 }
 
 function increment() {
@@ -35,50 +36,56 @@ function checkTime(m, s) {
   if (m == 0 && s == 15) {
     document.getElementById("step").innerHTML = "Give the slurry a gentle swirl.";
     sound.play();
-    goLight();
+    goDark();
   }
 
   if (m == 0 && s == 30) {
     document.getElementById("step").innerHTML = "Be still and let the coffee bloom.";
     sound.play();
-    goDark();
+    goLight();
   }
 
   if (m == 0 && s == 45) {
     document.getElementById("step").innerHTML = "Slowly pour water in a spiraling motion, up to the <span class=\"highlight\">300 grams</span> mark.";
     sound.play();
-    goLight();
+    document.getElementById("timer-box").style.transition = "background-color 30s linear, border-color 30s linear";
+    document.getElementById("start-btn").style.transition = "background-color 30s linear";
+    goDark();
   }
 
   if (m == 1 && s == 15) {
     document.getElementById("step").innerHTML = "And again, to the <span class=\"highlight\">500 grams</span> mark.";
     sound.play();
-    goDark();
+    goLight();
   }
 
   if (m == 1 && s == 45) {
     document.getElementById("step").innerHTML = "With a spoon, stir the grounds once clockwise and once counterclockwise.";
     sound.play();
-    goLight();
+    document.getElementById("timer-box").style.transition = "background-color 15s linear, border-color 15s linear";
+    document.getElementById("start-btn").style.transition = "background-color 15s linear";
+    goDark();
   }
 
   if (m == 2 && s == 00) {
     document.getElementById("step").innerHTML = "Give the dripper a gentle swirl.";
     sound.play();
-    goDark();
+    goLight();
   }
 
   if (m == 2 && s == 15) {
     document.getElementById("step").innerHTML = "Allow the brew to draw down.";
     // opportunity for some fun information here, while waiting
     sound.play();
-    goLight();
+    document.getElementById("timer-box").style.transition = "background-color 45s linear, border-color 45s linear";
+    document.getElementById("start-btn").style.transition = "background-color 45s linear";
+    goDark();
   }
 
   if (m == 3 && s == 00) {
     document.getElementById("step").innerHTML = "Good morning :)";
     sound.play();
-    goDark();
+    goLight();
     // how to stop the increment / setInterval at this point (ask rosa)
   }  
 }
@@ -89,17 +96,13 @@ function checkTime(m, s) {
 // switch to hario method?
 
 function goDark() {
-  document.getElementById("timer-box").style.backgroundColor = "#4B3218";
-  document.getElementById("timer-box").style.borderColor = "#4B3218";
-  document.getElementById("timer-box").style.color = "moccasin"; 
-  document.getElementById("start-btn").style.backgroundColor = "#4B3218";
-  document.getElementById("bean-1").style.fill = "moccasin";
+  document.getElementById("timer-box").style.backgroundColor = "var(--dark-color)";
+  document.getElementById("timer-box").style.borderColor = "var(--dark-color)";
+  document.getElementById("start-btn").style.backgroundColor = "var(--dark-color)";
 }
 
 function goLight() {
-  document.getElementById("timer-box").style.backgroundColor = "moccasin";
-  document.getElementById("timer-box").style.borderColor = "saddlebrown";
-  document.getElementById("timer-box").style.color = "saddlebrown"; 
-  document.getElementById("start-btn").style.backgroundColor = "moccasin";
-  document.getElementById("bean-1").style.fill = "saddlebrown";
+  document.getElementById("timer-box").style.backgroundColor = "var(--mid-color)";
+  document.getElementById("timer-box").style.borderColor = "var(--mid-color)";
+  document.getElementById("start-btn").style.backgroundColor = "var(--mid-color)";
 }
